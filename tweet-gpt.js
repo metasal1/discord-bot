08ca58e1c1c0df920bad904bfbb9a36e4282f63a
Salim Karim <imsjk8@gmail.com>
2023-05-12 14:17:15 +1000

    changed to 19 min intervals

--- tweet-gpt.js
+++ tweet-gpt.js
@@ -7,7 +7,7 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-const tweetSchedule = schedule.scheduleJob('10 * * * *', async function () {
+const tweetSchedule = schedule.scheduleJob('/30 * * * *', async function () {
 
     const access_token = process.env.TWITTER_ACCESS_TOKEN_SOLANA_FAQS;
     const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET_SOLANA_FAQS;
