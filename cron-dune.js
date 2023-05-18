0912eed800c525f10b29e0b09514dff26ba92c7c
Salim Karim <imsjk8@gmail.com>
2023-05-18 23:02:37 +1000

    cron-dune whoops

--- cron-dune.js
+++ cron-dune.js
@@ -3,22 +3,22 @@ import fetch from 'node-fetch';
 
 dotenv.config();
 
-// const tweetSchedule = schedule.scheduleJob('0 */3 * * *', async function () {
+const tweetSchedule = schedule.scheduleJob('0 */3 * * *', async function () {
 
-(async () => {
-    try {
-        const req = await fetch(`https://api.dune.com/api/v1/query/2476143/execute?api_key=${process.env.DUNE_API_KEY}`, {
-            method: 'POST',
-            headers: {
-                'content-type': "application/json",
-                'accept': "application/json"
-            }
-        });
+    (async () => {
+        try {
+            const req = await fetch(`https://api.dune.com/api/v1/query/2476143/execute?api_key=${process.env.DUNE_API_KEY}`, {
+                method: 'POST',
+                headers: {
+                    'content-type': "application/json",
+                    'accept': "application/json"
+                }
+            });
 
-        const res = await req.json();
-        console.log(res);
-    } catch (e) {
-        console.dir(e);
-    }
-})();
-// });
+            const res = await req.json();
+            console.log(res);
+        } catch (e) {
+            console.dir(e);
+        }
+    })();
+});
